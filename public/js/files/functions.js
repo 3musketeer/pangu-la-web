ebf34ba89c408ad7b729c5125d56f956b9b40800
tangsz <tangsz@asiainfo-linkage.com>
2014-07-02 19:12:30 +0800

    md

--- public/js/files/functions.js
+++ public/js/files/functions.js
@@ -751,6 +751,12 @@
    });
    
    $(document).on('complete.pjax', function (e) { 
+    	 $('#total-visits').sparkline(
+		       'html', {type: 'bar', barColor: '#ef705b', height: '35px', barWidth: "5px", barSpacing: "2px", zeroAxis: "false"}
+	     );
+	     $('#balance').sparkline(
+		       'html', {type: 'bar', barColor: '#91c950', height: '35px', barWidth: "5px", barSpacing: "2px", zeroAxis: "false"}
+	     );
        schedule();
    });
    //updating ctrol end
