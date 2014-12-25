5f9f0c2ef45da7c2e961392c2b5642209e2a9bd3
tangsz <tangsz@asiainfo-linkage.com>
2014-12-25 10:28:13 +0800

    md

--- app/controllers/plugin_config/hostCompare/config_history.js
+++ app/controllers/plugin_config/hostCompare/config_history.js
@@ -3,50 +3,50 @@ var scopeNames = {'day':'日', 'month':'月', 'year':'年'}
 exports.config = {
 	
 	TuxStateCalledSumByTimeAt28: {
-		name: '197调用总数',
+		name: '139调用总数',
 		scopes: ['day'],
 		scopeNames: scopeNames,
 		colNames : [ 'hours', '_count' ], 
-		filter: {SVRNAME: {$exists: false}, TRANSCODE:{$exists:false},host:'134.32.28.197'},
+		filter: {SVRNAME: {$exists: false}, TRANSCODE:{$exists:false},host:'134.32.28.139'},
 		color: "#ef705b",
 		filterColNames: [],
 		sort: {'hours' : 1}
 	},
 	TuxStateCalledSumByTimeAt29: {
-		name: '198调用总数',
+		name: '141调用总数',
 		scopes: ['day'],
 		scopeNames: scopeNames,
 		colNames : [ 'hours', '_count' ], 
-		filter: {SVRNAME: {$exists: false}, TRANSCODE:{$exists:false},host:'134.32.28.198'},
+		filter: {SVRNAME: {$exists: false}, TRANSCODE:{$exists:false},host:'134.32.28.141'},
 		color: "#4bb0ce",
 		filterColNames: [],
 		sort: {'hours' : 1}
 	},
 	
 	TuxStateFailedSumByTimeAt28: {
-		name: '197异常总数',
+		name: '139异常总数',
 		scopes: ['day'],
 		scopeNames: scopeNames,
 		colNames : [ 'hours', '_count' ], 
-		filter: {SVRNAME: {$exists: false}, TRANSCODE:{$exists:false},host:'134.32.28.197'},
+		filter: {SVRNAME: {$exists: false}, TRANSCODE:{$exists:false},host:'134.32.28.139'},
 		color: "#ef705b",
 		filterColNames: [],
 		sort: {'hours' : 1}
 	},
 
 	TuxStateFailedSumByTimeAt29: {
-		name: '198异常总数',
+		name: '141异常总数',
 		scopes: ['day'],
 		scopeNames: scopeNames,
 		colNames : [ 'hours', '_count' ], 
-		filter: {SVRNAME: {$exists: false}, TRANSCODE:{$exists:false},host:'134.32.28.198'},
+		filter: {SVRNAME: {$exists: false}, TRANSCODE:{$exists:false},host:'134.32.28.141'},
 		color: "#4bb0ce",
 		filterColNames: [],
 		sort: {'hours' : 1}
 	},
 	
 	TuxStateCalledSumByTimeBySvc1: {
-		name: '服务qcscrm1l1server调用总数',
+		name: 'qcscrm1l1',
 		scopes: ['day'],
 		scopeNames: scopeNames,
 		colNames : [ 'hours', '_count' ], 
@@ -56,11 +56,21 @@ exports.config = {
 		sort: {'hours' : 1}
 	},
 	TuxStateCalledSumByTimeBySvc2: {
-		name: '服务qcscrm1l2server调用总数',
+		name: 'tcscrm1l1',
 		scopes: ['day'],
 		scopeNames: scopeNames,
 		colNames : [ 'hours', '_count' ], 
-		filter: {TRANSCODE:{$exists:false},SVRNAME:'qcscrm1l2server'},
+		filter: {TRANSCODE:{$exists:false},SVRNAME:'tcscrm1l1server'},
+		color: "#4bb0ce",
+		filterColNames: [],
+		sort: {'hours' : 1}
+	},
+	TuxStateCalledSumByTimeBySvc3: {
+		name: 'qamcbs1l1',
+		scopes: ['day'],
+		scopeNames: scopeNames,
+		colNames : [ 'hours', '_count' ], 
+		filter: {TRANSCODE:{$exists:false},SVRNAME:'qamcbs1l1server'},
 		color: "#4bb0ce",
 		filterColNames: [],
 		sort: {'hours' : 1}
@@ -78,6 +88,7 @@ exports.list = {
 	                                ,{mode:'TuxState', type:'FailedSumByTime', subtype: 'At29',hostName: '异常数'}]
 	                           ,[   {mode:'TuxState', type:'CalledSumByTime', subtype: 'BySvc1',hostName: '服务调用数'}
 	                               ,{mode:'TuxState', type:'CalledSumByTime', subtype: 'BySvc2',hostName: '服务调用数'}
+	                               ,{mode:'TuxState', type:'CalledSumByTime', subtype: 'BySvc3',hostName: '服务调用数'}
 	                            ]
 	                            ]
 	                 
