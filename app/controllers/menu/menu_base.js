896de515ff31623f7cbdac5593fa2d961274875f
tangsz <tangsz@asiainfo-linkage.com>
2014-12-26 16:32:06 +0800

    md

--- app/controllers/menu/menu_base.js
+++ app/controllers/menu/menu_base.js
@@ -1,10 +1,9 @@
 module.exports = [
     {title:'业务',menuId:'current',class:'icon-signal',list:[    //{title:'工单积压',url:'/orderOverstock.html?chartList=lineChart'},
                                                 //{title:'错单',url:'/historyTopDetail.html?chartList=historyTopDetailList'},
-                                                {title:'流程超时(历史)',url:'/historyQueryDetail.html?chartList=historyTopDetailList'},
+                                                {title:'流程超时(日)',url:'/historyQueryDetail.html?chartList=historyTopDetailList'},
                                                 {title:'流程超时(实时)',url:'/realtimeQueryDetail.html?chartList=realtimeLcuTimeoutDetailList'},
-                                                {title:'流程调用总量(历史)',url:'/lcuCalledSum.html?chartList=lcuCalledSumList'}, 
-                                                {title:'超时流程分析',url:'/historyQueryDetail.html?chartList=lcuTimeTopAnalyseRate'},                
+                                                {title:'流程调用总量',url:'/lcuCalledSum.html?chartList=lcuCalledSumList'},      
                                                // {title:'流程调用总量(实时)',url:'/realtimeLcuCalledSum.html?chartList=realTimeLcuCalledSumChart'},                            
                                                 {title:'流程调用异常',url:'/lcuCalledSum.html?chartList=lcuFailedSumList'}
                                                // {title:'流程调用异常(实时)',url:'/realtimeLcuCalledSum.html?chartList=realTimeLcuFailSumChart'},
@@ -13,21 +12,29 @@ module.exports = [
                                             ]
     },
    
-    {title:'中间件TUXEDO',menuId:'tuxedo', class:'icon-tasks',list:[ //{title:'问题定位',url:'/flowInfoAnalysis.html?flowId=/lcuErrorAnalysis.html'},
-                                                    {title:'主机对比',url:'/historyComPareGraph.html?chartList=historyLcuSumCompareChart'}, 
+    {title:'中间件',menuId:'tuxedo', class:'icon-tasks',list:[ //{title:'问题定位',url:'/flowInfoAnalysis.html?flowId=/lcuErrorAnalysis.html'},
                                                     //{title:'主机对比(实时)',url:'/realtimeHostCompareGraph.html?chartList=realTimeLcuSumCompareChart'},
-                                                    {title:'队列分析',url:'/queueAnalyze.html'},
                                                     {title:'服务队列(历史)',url:'/queueMonitorHis.html'},
                                                     {title:'服务队列(实时)',url:'/queueMonitor.html'},
                                                     //{title:'服务僵死',url:'/realtimeQueryDetail.html?chartList=realTimeSvcDeadDetailList'},                                            
                                                     {title:'内存占用',url:'/memoryMonitor.html?chartList=realMemory'},
-                                                    {title:'服务超时(历史)',url:'/historyQueryDetail.html?chartList=historySvcTimeOutDetailList'},     
-                                                    {title:'服务超时(实时)',url:'/realtimeQueryDetail.html?chartList=realTimeSvcTimeOutDetailList'},      
+                                                    {title:'服务超时明细(日)',url:'/historyQueryDetail.html?chartList=historySvcTimeOutDetailList'},     
+                                                    {title:'服务超时明细(实时)',url:'/realtimeQueryDetail.html?chartList=realTimeSvcTimeOutDetailList'},      
                                                     {title:'服务调用总量',url:'/lcuCalledSum.html?chartList=svcCalledSumList'},    
                                                     //{title:'服务调用总量(实时)',url:'/realtimeLcuCalledSum.html?chartList=realTimeSvcCalledSumChart'},  
                                                     {title:'服务调用异常',url:'/lcuCalledSum.html?chartList=svcFailedSumList'},  
                                                     //{title:'服务调用异常(实时)',url:'/realtimeLcuCalledSum.html?chartList=realTimeSvcFailedSumChart'},  
                                                     //{title:'服务调用成功率',url:'/sumList.html?chartList=svcCalledSumChart'}    
+                                                    
+                                                  ]
+    },
+    
+    
+    {title:'分析',menuId:'analysis', class:'icon-tasks',list:[
+                                                    {title:'超时流程分析',url:'/historyQueryDetail.html?chartList=lcuTimeTopAnalyseRate'},
+                                                    {title:'队列分析',url:'/queueAnalyze.html'},   
+                                                    {title:'主机对比',url:'/historyComPareGraph.html?chartList=historyLcuSumCompareChart'}
+                                                    
                                                     
                                                   ]
     },
